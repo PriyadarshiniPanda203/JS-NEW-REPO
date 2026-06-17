b7f4a8bc3643088c5b0e37785a82e2ed1de4ac55
PriyadarshiniPanda203 <Priyadarshinipanda2138@gmail.com>
2026-06-17 07:37:36 +0000

    js datatypes

---
+++ JS_Basics/datatypes.js
@@ -0,0 +1,19 @@
+// datatypes 
+let name = " priyadarshini "; // string
+let age = 22; // numeric 
+// let have_insta= no;// RefferenceError: no is not defined
+let have_insta =0;    
+let have_fb = false;     
+let state = null;
+let condition;
+console.table([name , age , have_insta , state , condition]);
+
+// to know the datatypes of these variables we need to use "typeof ()"
+
+console.log(typeof condition); // undefined dt
+console.log(typeof state);     // object
+console.log(typeof have_insta); // number
+console.log(typeof name);       //string 
+console.log(typeof age);        //  number
+console.log(typeof have_fb);    // boolean 
+
